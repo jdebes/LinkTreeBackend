db6dd1c8acdc2c8cf51bfad682a8aaeda21f9ad6
Jonas Debes <jpdebes@gmail.com>
2021-07-05 09:46:36 +1200

    Remove db so that server runs w/o mysql

--- server/server.go
+++ server/server.go
@@ -6,46 +6,30 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/jdebes/LinkTreeBackend/db"
 	"github.com/jdebes/LinkTreeBackend/db/repository"
 	"github.com/jdebes/LinkTreeBackend/handler"
-	"github.com/jmoiron/sqlx"
 	log "github.com/sirupsen/logrus"
 )
 
 type RootHandler struct {
 	router    *mux.Router
-	db        *sqlx.DB
 	mockStore *repository.MockStore
 }
 
 func (f *RootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	ctx = db.WithDB(ctx, f.db)
 	ctx = repository.WithStore(ctx, f.mockStore)
 
 	f.router.ServeHTTP(w, r.WithContext(ctx))
 }
 
 func NewServer() *http.Server {
-	sqlDb, err := db.NewDB(db.Config{
-		User:     "user",
-		Password: "password",
-		Host:     "127.0.0.1",
-		Port:     3306,
-		Database: "linktree",
-	})
-	if err != nil {
-		panic(err)
-	}
-
 	mockStore := repository.NewMockStore()
 
 	return &http.Server{
 		Addr: ":8080",
 		Handler: &RootHandler{
 			router:    newRouter(),
-			db:        sqlDb,
 			mockStore: mockStore,
 		},
 		ReadTimeout:       15 * time.Second,
